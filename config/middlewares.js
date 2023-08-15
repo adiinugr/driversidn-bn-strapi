60917bd4ebfebbeabf48c8766ea7715986abafcb
Adi Nugroho <addieze@ymail.com>
2023-08-15 12:31:27 +0700

    remove upload setting

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,31 +1,6 @@
 module.exports = [
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.digitaloceanspaces.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.digitaloceanspaces.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
