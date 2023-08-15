796a67d31128f3e954785464ac27eaffd90e6d0c
Adi Nugroho <addieze@ymail.com>
2023-08-15 22:33:16 +0700

    change plugin upload options 4

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,20 +7,8 @@ module.exports = [
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.digitaloceanspaces.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "dl.airtable.com",
-            "*.digitaloceanspaces.com",
-          ],
+          "img-src": ["'self'", "data:", "blob:", "*.digitaloceanspaces.com"],
+          "media-src": ["'self'", "data:", "blob:"],
           upgradeInsecureRequests: null,
         },
       },
@@ -31,7 +19,6 @@ module.exports = [
   "strapi::logger",
   "strapi::query",
   "strapi::body",
-  "strapi::session",
   "strapi::favicon",
   "strapi::public",
 ];
