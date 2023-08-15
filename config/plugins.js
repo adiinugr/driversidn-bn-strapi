64fa89a03107750c5f7b6e960a687f5bdee9603c
Adi Nugroho <addieze@ymail.com>
2023-08-15 14:20:25 +0700

    change do uplaod setting

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,19 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "strapi-provider-upload-do",
+      provider: "aws-s3",
       providerOptions: {
-        key: env("DO_SPACE_ACCESS_KEY"),
-        secret: env("DO_SPACE_SECRET_KEY"),
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
-        space: env("DO_SPACE_BUCKET"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
